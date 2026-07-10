68f50651171196585a549d691005b5811b9b5e4d
jeenicj97 <jeenicj97@gmail.com>
2026-07-10 09:35:19 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Flask PostgreSQL Docker Project
+
+## Description
+Simple Flask application running with PostgreSQL using Docker Compose.
+
+## Services
+- Flask Application
+- PostgreSQL Database
+
+## Start
+
+docker compose up -d --build
+
+## Stop
+
+docker compose down
+
+## Environment Variables
+
+POSTGRES_DB
+POSTGRES_USER
+POSTGRES_PASSWORD
+DB_HOST
+DB_NAME
+DB_USER
+DB_PASSWORD
+
+## Application URL
+
+http://localhost:5000
---
+++ app/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+
+@app.route("/")
+def home():
+    return "Hello from Flask + PostgreSQL Docker Project!"
+
+
+@app.route("/health")
+def health():
+    return "Application is running"
+
+
+app.run(host="0.0.0.0", port=5000)
